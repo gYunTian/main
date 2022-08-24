90255739f3926e33c1ad0e8b931f052502db63ed
gYunTian <54625060+gYunTian@users.noreply.github.com>
2022-08-24 13:47:45 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved. All Rights Reserved.
+
+from .build import build_model, MODEL_REGISTRY  # noqa
+from .model import MViT  # noqa
