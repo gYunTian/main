fd7c889961cab9c5d92913179545cb5bc6855275
gYunTian <54625060+gYunTian@users.noreply.github.com>
2022-08-26 19:20:29 +0800

    lazy fix for torchscript

--- models/attention.py
+++ models/attention.py
@@ -5,49 +5,78 @@
 import numpy
 import torch
 import torch.nn as nn
-from main.models.common import DropPath, Mlp
+from mvit.models.common import DropPath, Mlp
 from torch.nn.init import trunc_normal_
+from typing import List, Dict, Tuple
 
+class Conv(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int, kernel: List[int], 
+    stride: List[int], padding: List[int], groups: int, bias: bool) -> None:
+        super().__init__()
+        print("In Conv module")
+        self.conv = nn.Conv2d(in_channels=in_dim, out_channels=out_dim, 
+        kernel_size=kernel, stride=stride, padding=padding, groups=groups, bias=bias)
+        print("created Conv")
 
-def attention_pool(tensor, pool, hw_shape, has_cls_embed=True, norm=None):
-    if pool is None:
-        return tensor, hw_shape
-    tensor_dim = tensor.ndim
-    if tensor_dim == 4:
-        pass
-    elif tensor_dim == 3:
-        tensor = tensor.unsqueeze(1)
-    else:
-        raise NotImplementedError(f"Unsupported input dimension {tensor.shape}")
+    # def forward(self, x: torch.Tensor) -> torch.Tensor:
+    #     return self.conv(x)
 
-    if has_cls_embed:
-        cls_tok, tensor = tensor[:, :, :1, :], tensor[:, :, 1:, :]
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.conv(x)
 
-    B, N, L, C = tensor.shape
-    H, W = hw_shape[0], hw_shape[1]
-    tensor = tensor.reshape(B * N, H, W, C).permute(0, 3, 1, 2).contiguous()
+# def attention_pool(tensor: torch.Tensor, pool, hw_shape: List[int], has_cls_embed=True, norm=None):
+#     if pool is None:
+#         return tensor, hw_shape
+#     elif isinstance(pool, Dict):
+#         print("Found dict, IN pool:", tensor.shape)
+#         # pool = torch.nn.functional.conv2d(weight=pool['kernel'], stride=pool['stride'], padding=pool['padding'],groups=pool['groups'])
+#         print(pool)
+#         pool = nn.Conv2d(
+#             pool['dim_conv'],
+#             pool['dim_conv'],
+#             pool['kernel'],
+#             stride=pool['stride'],
+#             padding=pool['padding'],
+#             groups=pool['groups'],
+#             bias=pool['bias'],
+#             ).to("cuda:0")
+    
+#     tensor_dim = tensor.ndim
+#     if tensor_dim == 4:
+#         pass
+#     elif tensor_dim == 3:
+#         tensor = tensor.unsqueeze(1)
+#     else:
+#         raise NotImplementedError(f"Unsupported input dimension {tensor.shape}")
 
-    tensor = pool(tensor)
+#     if has_cls_embed:
+#         cls_tok, tensor = tensor[:, :, :1, :], tensor[:, :, 1:, :]
 
-    hw_shape = [tensor.shape[2], tensor.shape[3]]
-    L_pooled = tensor.shape[2] * tensor.shape[3]
-    tensor = tensor.reshape(B, N, C, L_pooled).transpose(2, 3)
-    if has_cls_embed:
-        tensor = torch.cat((cls_tok, tensor), dim=2)
-    if norm is not None:
-        tensor = norm(tensor)
+#     B, N, L, C = tensor.shape
+#     H, W = hw_shape[0], hw_shape[1]
+#     tensor = tensor.reshape(B * N, H, W, C).permute(0, 3, 1, 2).contiguous()
 
-    if tensor_dim == 3:
-        tensor = tensor.squeeze(1)
-    return tensor, hw_shape
+#     tensor = pool(tensor)
+
+#     hw_shape = [tensor.shape[2], tensor.shape[3]]
+#     L_pooled = tensor.shape[2] * tensor.shape[3]
+#     tensor = tensor.reshape(B, N, C, L_pooled).transpose(2, 3)
+#     if has_cls_embed:
+#         tensor = torch.cat((cls_tok, tensor), dim=2)
+#     if norm is not None:
+#         tensor = norm(tensor)
+
+#     if tensor_dim == 3:
+#         tensor = tensor.squeeze(1)
+#     return tensor, hw_shape
 
 
 def cal_rel_pos_spatial(
-    attn,
-    q,
-    has_cls_embed,
-    q_shape,
-    k_shape,
+    attn: torch.Tensor,
+    q: torch.Tensor,
+    has_cls_embed: bool,
+    q_shape: List[int],
+    k_shape: List[int],
     rel_pos_h,
     rel_pos_w,
 ):
@@ -55,6 +84,8 @@ def cal_rel_pos_spatial(
     Spatial Relative Positional Embeddings.
     """
     sp_idx = 1 if has_cls_embed else 0
+    print("Q shape:", q_shape)
+    print("K shape:", k_shape)
     q_h, q_w = q_shape
     k_h, k_w = k_shape
 
@@ -121,6 +152,10 @@ class MultiScaleAttention(nn.Module):
         padding_q = [int(q // 2) for q in kernel_q]
         padding_kv = [int(kv // 2) for kv in kernel_kv]
 
+        self.q = nn.Linear(0, 0)
+        self.k = nn.Linear(0, 0)
+        self.v = nn.Linear(0, 0)
+        self.proj = nn.Linear(0, 0)
         if pool_first:
             self.q = nn.Linear(dim, dim_out, bias=qkv_bias)
             self.k = nn.Linear(dim, dim_out, bias=qkv_bias)
@@ -131,8 +166,10 @@ class MultiScaleAttention(nn.Module):
         self.proj = nn.Linear(dim_out, dim_out)
 
         # Skip pooling with kernel and stride size of (1, 1, 1).
+        #if torch.prod(kernel_q) == 1 and torch.prod(stride_q) == 1:
         if numpy.prod(kernel_q) == 1 and numpy.prod(stride_q) == 1:
             kernel_q = ()
+        #if torch.prod(kernel_kv) == 1 and torch.prod(stride_kv) == 1:
         if numpy.prod(kernel_kv) == 1 and numpy.prod(stride_kv) == 1:
             kernel_kv = ()
         self.mode = mode
@@ -159,6 +196,14 @@ class MultiScaleAttention(nn.Module):
                 dim_conv = dim // num_heads if mode == "conv" else dim
             else:
                 dim_conv = dim_out // num_heads if mode == "conv" else dim_out
+            
+            # self.pool_q = Conv(in_dim=dim_conv, 
+            # out_dim=dim_conv, 
+            # kernel=kernel_q, 
+            # stride=stride_q, 
+            # padding=padding_q, 
+            # groups=dim_conv, 
+            # bias=False) if len(kernel_q) > 0 else None
             self.pool_q = (
                 nn.Conv2d(
                     dim_conv,
@@ -173,6 +218,14 @@ class MultiScaleAttention(nn.Module):
                 else None
             )
             self.norm_q = norm_layer(dim_conv) if len(kernel_q) > 0 else None
+
+            # self.pool_k = Conv(in_dim=dim_conv, 
+            # out_dim=dim_conv, 
+            # kernel=kernel_kv, 
+            # stride=stride_kv, 
+            # padding=padding_kv, 
+            # groups=dim_conv, 
+            # bias=False) if len(kernel_q) > 0 else None
             self.pool_k = (
                 nn.Conv2d(
                     dim_conv,
@@ -187,6 +240,14 @@ class MultiScaleAttention(nn.Module):
                 else None
             )
             self.norm_k = norm_layer(dim_conv) if len(kernel_kv) > 0 else None
+
+            # self.pool_v = Conv(in_dim=dim_conv, 
+            # out_dim=dim_conv, 
+            # kernel=kernel_kv, 
+            # stride=stride_kv, 
+            # padding=padding_kv, 
+            # groups=dim_conv, 
+            # bias=False) if len(kernel_q) > 0 else None
             self.pool_v = (
                 nn.Conv2d(
                     dim_conv,
@@ -223,86 +284,143 @@ class MultiScaleAttention(nn.Module):
 
         self.residual_pooling = residual_pooling
 
-#     def forward(self, x, hw_shape):
-#         B, N, _ = x.shape
+    def concat(self, x: List[torch.Tensor], dim: int) -> torch.Tensor:
+        return torch.cat(x, dim=dim)
 
-#         if self.pool_first:
-#             if self.mode == "conv_unshared":
-#                 fold_dim = 1
-#             else:
-#                 fold_dim = self.num_heads
-#             x = x.reshape(B, N, fold_dim, -1).permute(0, 2, 1, 3)
-#             q = k = v = x
-#         else:
-#             assert self.mode != "conv_unshared"
+    def attention_pool(self, tensor: torch.Tensor, type: str, hw_shape: List[int], has_cls_embed: bool=True, norm: bool=False):
+        # if type == 'q' : pool = self.pool_q
+        # elif type == 'k': pool = self.pool_k
+        # elif type == 'v': pool = self.pool_v
+        if type is None: return tensor, hw_shape
+        # if pool is None: return tensor, hw_shape
+        
+        tensor_dim = tensor.ndim
+        if tensor_dim == 4:
+            pass
+        elif tensor_dim == 3:
+            tensor = tensor.unsqueeze(1)
+        else:
+            raise NotImplementedError(f"Unsupported input dimension {tensor.shape}")
 
-#             qkv = (
-#                 self.qkv(x).reshape(B, N, 3, self.num_heads, -1).permute(2, 0, 3, 1, 4)
-#             )
-#             q, k, v = qkv[0], qkv[1], qkv[2]
+        if has_cls_embed:
+            cls_tok, tensor = tensor[:, :, :1, :], tensor[:, :, 1:, :]
+        else: cls_tok = torch.empty(1)
 
-#         q, q_shape = attention_pool(
-#             q,
-#             None,
-#             #self.pool_q,
-#             hw_shape,
-#             has_cls_embed=self.has_cls_embed,
-#             norm=self.norm_q if hasattr(self, "norm_q") else None,
-#         )
-#         k, k_shape = attention_pool(
-#             k,
-#             None,
-#             #self.pool_k,
-#             hw_shape,
-#             has_cls_embed=self.has_cls_embed,
-#             norm=self.norm_k if hasattr(self, "norm_k") else None,
-#         )
-#         v, v_shape = attention_pool(
-#             v,
-#             None,
-#             #self.pool_v,
-#             hw_shape,
-#             has_cls_embed=self.has_cls_embed,
-#             norm=self.norm_v if hasattr(self, "norm_v") else None,
-#         )
+        B, N, L, C = tensor.shape
+        H, W = hw_shape
+        tensor = tensor.reshape(B * N, H, W, C).permute(0, 3, 1, 2).contiguous()
 
-#         if self.pool_first:
-#             q_N = numpy.prod(q_shape) + 1 if self.has_cls_embed else numpy.prod(q_shape)
-#             k_N = numpy.prod(k_shape) + 1 if self.has_cls_embed else numpy.prod(k_shape)
-#             v_N = numpy.prod(v_shape) + 1 if self.has_cls_embed else numpy.prod(v_shape)
+        if type == 'q' : tensor = self.pool_q(tensor)
+        elif type == 'k': tensor = self.pool_k(tensor)
+        else: tensor = self.pool_v(tensor)
+        #tensor = pool(tensor)
 
-#             q = q.permute(0, 2, 1, 3).reshape(B, q_N, -1)
-#             q = self.q(q).reshape(B, q_N, self.num_heads, -1).permute(0, 2, 1, 3)
+        hw_shape = [tensor.shape[2], tensor.shape[3]]
+        L_pooled = tensor.shape[2] * tensor.shape[3]
+        tensor = tensor.reshape(B, N, C, L_pooled).transpose(2, 3)
+        if has_cls_embed:
+            tensor = self.concat([cls_tok, tensor], dim=2)
+            #tensor = torch.cat([cls_tok, tensor], dim=2)
+        
+        if norm:
+            if hasattr(self, "norm_q"): tensor = self.norm_q(tensor)
+            elif hasattr(self, "norm_k"): tensor = self.norm_k(tensor)
+            elif hasattr(self, "norm_v"): tensor = self.norm_v(tensor)
+            else: tensor = tensor
+        
+        if tensor_dim == 3:
+            tensor = tensor.squeeze(1)
+        return tensor, hw_shape
 
-#             v = v.permute(0, 2, 1, 3).reshape(B, v_N, -1)
-#             v = self.v(v).reshape(B, v_N, self.num_heads, -1).permute(0, 2, 1, 3)
+    def forward(self, x, hw_shape: List[int]):
+        B, N, _ = x.shape
 
-#             k = k.permute(0, 2, 1, 3).reshape(B, k_N, -1)
-#             k = self.k(k).reshape(B, k_N, self.num_heads, -1).permute(0, 2, 1, 3)
+        if self.pool_first:
+            if self.mode == "conv_unshared":
+                fold_dim = 1
+            else:
+                fold_dim = self.num_heads
+            x = x.reshape(B, N, fold_dim, -1).permute(0, 2, 1, 3)
+            q = k = v = x
+        else:
+            assert self.mode != "conv_unshared"
 
-#         N = q.shape[2]
-#         attn = (q * self.scale) @ k.transpose(-2, -1)
-#         if self.rel_pos_spatial:
-#             attn = cal_rel_pos_spatial(
-#                 attn,
-#                 q,
-#                 self.has_cls_embed,
-#                 q_shape,
-#                 k_shape,
-#                 self.rel_pos_h,
-#                 self.rel_pos_w,
-#             )
+            qkv = (
+                self.qkv(x).reshape(B, N, 3, self.num_heads, -1).permute(2, 0, 3, 1, 4)
+            )
+            q, k, v = qkv[0], qkv[1], qkv[2]
 
-#         attn = attn.softmax(dim=-1)
-#         x = attn @ v
+        q, q_shape = self.attention_pool(
+            q,
+            'q',
+            #self.pool_q,
+            hw_shape,
+            has_cls_embed=self.has_cls_embed,
+            norm=True
+            #norm=self.norm_q if hasattr(self, "norm_q") else None,
+        )
+        k, k_shape = self.attention_pool(
+            k,
+            'k',
+            #self.pool_k,
+            hw_shape,
+            has_cls_embed=self.has_cls_embed,
+            norm=True
+            #norm=self.norm_k if hasattr(self, "norm_k") else None,
+        )
+        v, v_shape = self.attention_pool(
+            v,
+            'v',
+            #self.pool_v,
+            hw_shape,
+            has_cls_embed=self.has_cls_embed,
+            norm=True
+            #norm=self.norm_v if hasattr(self, "norm_v") else None,
+        )
 
-#         if self.residual_pooling:
-#             x = x + q
+        if self.pool_first:
+            qs = torch.tensor(q_shape)
+            q_N = torch.prod(qs) + 1 if self.has_cls_embed else torch.prod(qs)
+            #q_N = numpy.prod(q_shape) + 1 if self.has_cls_embed else numpy.prod(q_shape)
+            ks = torch.tensor(k_shape)
+            k_N = torch.prod(ks) + 1 if self.has_cls_embed else torch.prod(ks)
+            # k_N = numpy.prod(k_shape) + 1 if self.has_cls_embed else numpy.prod(k_shape)
+            vs = torch.tensor(v_shape)
+            v_N = torch.prod(vs) + 1 if self.has_cls_embed else torch.prod(vs)
+            # v_N = numpy.prod(v_shape) + 1 if self.has_cls_embed else numpy.prod(v_shape)
 
-#         x = x.transpose(1, 2).reshape(B, -1, self.dim_out)
-#         x = self.proj(x)
+            q = q.permute(0, 2, 1, 3).reshape(B, q_N, -1)
+            q = self.q(q).reshape(B, q_N, self.num_heads, -1).permute(0, 2, 1, 3)
 
-#         return x, q_shape
+            v = v.permute(0, 2, 1, 3).reshape(B, v_N, -1)
+            v = self.v(v).reshape(B, v_N, self.num_heads, -1).permute(0, 2, 1, 3)
+
+            k = k.permute(0, 2, 1, 3).reshape(B, k_N, -1)
+            k = self.k(k).reshape(B, k_N, self.num_heads, -1).permute(0, 2, 1, 3)
+
+        N = q.shape[2]
+        attn = (q * self.scale) @ k.transpose(-2, -1)
+        if self.rel_pos_spatial:
+            attn = cal_rel_pos_spatial(
+                attn,
+                q,
+                self.has_cls_embed,
+                q_shape,
+                k_shape,
+                self.rel_pos_h,
+                self.rel_pos_w,
+            )
+
+        attn = attn.softmax(dim=-1)
+        x = attn @ v
+
+        if self.residual_pooling:
+            x = x + q
+
+        x = x.transpose(1, 2).reshape(B, -1, self.dim_out)
+        x = self.proj(x)
+
+        return x, q_shape
 
 
 class MultiScaleBlock(nn.Module):
@@ -364,10 +482,12 @@ class MultiScaleBlock(nn.Module):
             hidden_features=mlp_hidden_dim,
             out_features=mlp_dim_out,
         )
+        self.proj = nn.Linear(0, 0)
         if dim != dim_out:
             self.proj = nn.Linear(dim, dim_out)
 
         if len(stride_q) > 0 and numpy.prod(stride_q) > 1:
+        #if len(stride_q) > 0 and torch.prod(stride_q) > 1:
             kernel_skip = [s + 1 if s > 1 else s for s in stride_q]
             stride_skip = stride_q
             padding_skip = [int(skip // 2) for skip in kernel_skip]
@@ -376,15 +496,61 @@ class MultiScaleBlock(nn.Module):
             )
         else:
             self.pool_skip = None
+    
+    def concat(self, x: List[torch.Tensor], dim: int) -> torch.Tensor:
+        return torch.cat(x, dim=dim)
 
-    def forward(self, x, hw_shape):
+    def attention_pool(self, tensor: torch.Tensor, hw_shape: List[int], has_cls_embed: bool=True, norm: bool=False):
+        if self.pool_skip is None: return tensor, hw_shape
+        
+        tensor_dim = tensor.ndim
+        if tensor_dim == 4:
+            pass
+        elif tensor_dim == 3:
+            tensor = tensor.unsqueeze(1)
+        else:
+            raise NotImplementedError(f"Unsupported input dimension {tensor.shape}")
+
+        if has_cls_embed:
+            cls_tok, tensor = tensor[:, :, :1, :], tensor[:, :, 1:, :]
+        else: cls_tok = torch.empty(1)
+
+        B, N, L, C = tensor.shape
+        H, W = hw_shape
+        tensor = tensor.reshape(B * N, H, W, C).permute(0, 3, 1, 2).contiguous()
+
+        tensor = self.pool_skip(tensor)
+        #tensor = pool(tensor)
+
+        hw_shape = [tensor.shape[2], tensor.shape[3]]
+        L_pooled = tensor.shape[2] * tensor.shape[3]
+        tensor = tensor.reshape(B, N, C, L_pooled).transpose(2, 3)
+        if has_cls_embed:
+            tensor = self.concat([cls_tok, tensor], dim=2)
+            #tensor = torch.cat([cls_tok, tensor], dim=2)
+        
+        if norm:
+            if hasattr(self, "norm_q"): tensor = self.norm_q(tensor)
+            elif hasattr(self, "norm_k"): tensor = self.norm_k(tensor)
+            elif hasattr(self, "norm_v"): tensor = self.norm_v(tensor)
+            else: tensor = tensor
+        
+        if tensor_dim == 3:
+            tensor = tensor.squeeze(1)
+        return tensor, hw_shape
+
+    def forward(self, x, hw_shape: List[int]):
+        print("HW shape:", hw_shape)
         x_norm = self.norm1(x)
         x_block, hw_shape_new = self.attn(x_norm, hw_shape)
 
         if self.dim_mul_in_att and self.dim != self.dim_out:
             x = self.proj(x_norm)
-        x_res, _ = attention_pool(
-            x, None, hw_shape, has_cls_embed=self.has_cls_embed
+        # x_res, _ = self.attention_pool(
+        #     x, self.pool_skip, hw_shape, has_cls_embed=self.has_cls_embed
+        # )
+        x_res, _ = self.attention_pool(
+            x, hw_shape, has_cls_embed=self.has_cls_embed
         )
         x = x_res + self.drop_path(x_block)
         x_norm = self.norm2(x)
